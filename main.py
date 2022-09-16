ad30fc27607a29634d3b31c8df51994e0f48cad5
Egor <bamditse@gmail.com>
2022-09-16 22:04:15 +0700

    Added multimethods

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from container import (
     container_clear,
     container_sort,
     container_write_two_dimensional_array_to,
+    container_check_matrices,
     Container
 )
 
@@ -41,6 +42,7 @@ def main():
 
     # container_write_two_dimensional_array_to(cont, output_file)
     container_write_to(cont, output_file)
+    container_check_matrices(cont)
 
     container_clear(cont)
 
